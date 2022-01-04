09498380489583f5f46e7ca5c30183c57f3a7a33
Salman Setiawan <11191066@student.itk.ac.id>
2022-01-04 14:05:38 +0800

    improve landing page

--- src/test/TestPage.jsx
+++ src/test/TestPage.jsx
@@ -22,11 +22,12 @@ import MenuHealthTest from '../pages/MenuHealthTest'
 import ExampleHasilHealthTest from '../pages/ExampleHasilHealthTest'
 import MobilePhoneTable from '../pages/MobilePhoneTable'
 import Register3 from '../pages/Register3'
+import Landinginfo from '../pages/Landinginfo'
 
 const TestPage = () => {
     return (
         <div>
-            <Landingpage />
+            <Landinginfo />
         </div>
     )
 }
