99d5c1948925eccc468e2791d4ab234834cecd4c
Salman Setiawan <11191066@student.itk.ac.id>
2022-01-07 17:09:41 +0800

    kasih hover di blog

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const BlogCard = (props) => {
     return (
         <div>
-            <div className="rounded-lg bg-button md:w-96">
-                <img className='rounded-t-lg w-full' src={props.pic} alt="" />
-                <div className="flex flex-col p-4">
+            <div className="rounded bg-button md:w-96 cursor-pointer hover:bg-pink-600 hover:text-gray-50">
+                <div className="">
+                <img className='rounded-t w-full h-36 object-cover' src={props.pic} />  
+                </div>
+                <div className="flex flex-col px-4 py-5">
                     <div className="text-sm font-bold">
                         {props.title}
                     </div>
-                    <div className="mt-1 text-xxs text-gray-600">
+                    <div className="mt-1 text-xxs">
                         {props.desc}
                     </div>
                 </div>
